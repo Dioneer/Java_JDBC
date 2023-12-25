2af9bc0a20d765807aa0037709a6ff1053df0f38
Dioneer <gulkina_em@mail.ru>
2023-12-25 23:41:32 +0300

    connection

--- src/main/java/Pegas/JdbcRunner.java
+++ src/main/java/Pegas/JdbcRunner.java
@@ -2,11 +2,17 @@ package Pegas;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
     public static void main(String[] args){
-        try(Connection connection = ConnectionManager.open()){
-            System.out.println(connection.getTransactionIsolation());
+        String sql = """
+                insert into game.info(data) values ('2017-08-15'),
+                ('2018-08-15'),('2019-08-15'),('2020-08-15'),('2021-08-15')
+                """;
+        try(Connection connection = ConnectionManager.open();
+        Statement statement = connection.createStatement()){
+            System.out.println(statement.execute(sql));
         }catch(SQLException ex) {System.out.println(ex.getMessage());}
 
     }
